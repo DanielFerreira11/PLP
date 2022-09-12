cac0ffa1f5b7dd533851f66f49168bfa2aee6aaf
Daniel Ferreira Alves <51489376+DanielFerreira11@users.noreply.github.com>
2022-09-12 16:39:13 -0300

    Delete conversaocasamentodepadrao.pl

--- conversaocasamentodepadrao.pl
+++
@@ -1,9 +0,0 @@
-conversao(X,M,R):- M =@= volume, R is X * 3.785.
-conversao(X,M,R):- M =@= peso, R is X / 2.205.
-conversao(X,M,R):- M =@= distancia, R is X * 1.609.
-
-main:-
-read(X),
-read(M),
-conversao(X,M,R),
-format('~2f',R).
